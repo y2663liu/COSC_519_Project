90c1b3160351ed63f5fad439fb98951727660e6f
Mark Liu <y2663liu@uwaterloo.ca>
2025-10-12 20:38:55 -0700

    add camera and controller object

--- .utmp/RelWithDebInfo/4e3g666v/arm64-v8a/CMakeFiles/3.22.1-g37088a8-dirty/CMakeSystem.cmake
+++ .utmp/RelWithDebInfo/4e3g666v/arm64-v8a/CMakeFiles/3.22.1-g37088a8-dirty/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Windows")
 set(CMAKE_HOST_SYSTEM_VERSION "10.0.26100")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("E:/UnityEditor/6000.2.4f1/Editor/Data/PlaybackEngines/AndroidPlayer/NDK/build/cmake/android.toolchain.cmake")
+include("E:/UnityEditor/6000.2.7f2/Editor/Data/PlaybackEngines/AndroidPlayer/NDK/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-1")
 set(CMAKE_SYSTEM_NAME "Android")
