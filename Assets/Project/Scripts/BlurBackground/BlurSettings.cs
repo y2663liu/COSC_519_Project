0a1469ff98bba8224bf5ca4039aedb55d802df67
bindeel <binderupe@gmail.com>
2025-11-25 12:51:05 -0800

    Installing post processing package and creating new scripts for Blur

---
+++ Assets/Project/Scripts/BlurBackground/BlurSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[System.Serializable, VolumeComponentMenu("Blue")]
+public class BlurSettings : VolumeComponent, IPostProcessComponent
+{
+    
+}
