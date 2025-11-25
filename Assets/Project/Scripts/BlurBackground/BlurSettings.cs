e1e7ef349f215592fde188b5f258a371d20b35e9
bindeel <binderupe@gmail.com>
2025-11-25 14:24:14 -0800

    Adding scripts for blur effect

--- Assets/Project/Scripts/BlurBackground/BlurSettings.cs
+++ Assets/Project/Scripts/BlurBackground/BlurSettings.cs
@@ -5,5 +5,17 @@ using UnityEngine.Rendering.Universal;
 [System.Serializable, VolumeComponentMenu("Blue")]
 public class BlurSettings : VolumeComponent, IPostProcessComponent
 {
+    [Tooltip("Standard deviation (spread) of the blue Grid size is approx. 3x largers.")]
+    public ClampedFloatParameter strength = new ClampedFloatParameter(0.0f, 0.0f, 15.0f);
+
+    public bool IsActive()
+    {
+        return (strength.value > 0.0f) && active;
+    }
+
+    public bool IsTileCompatible()
+    {
+        return false;
+    }
     
 }
