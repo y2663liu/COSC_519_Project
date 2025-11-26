72fd11fc2d279ebe1e5efa240e303151748e8ecf
Echo1876-UBC <chenlei9411@163.com>
2025-11-26 12:52:02 -0800

    feat:add interaction of map

--- Assets/Project/Scripts/Interactables/HoverClue.cs
+++ Assets/Project/Scripts/Interactables/HoverClue.cs
@@ -1,31 +1,51 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class HoverClue : InteractableBase {
+public class HoverClue : InteractableBase
+{
     [SerializeField] string title;
     [SerializeField] string clues;
     [SerializeField] string funFacts;
 
     private string HintMessage;
-    
+
     [SerializeField] XRBaseInteractable rayInteractable;
 
-    protected void Start() {
+    private AudioSource audioSource;         
+    [SerializeField] private AudioClip audioClip;
+
+    protected void Start()
+    {
         rayInteractable.hoverEntered.AddListener(OnHoverEntered);
         rayInteractable.hoverExited.AddListener(OnHoverExited);
+
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
     }
 
-    public void OnHoverEntered(HoverEnterEventArgs args) {
+    public void OnHoverEntered(HoverEnterEventArgs args)
+    {
         HintPopup.Instance?.ShowHint(title, clues, funFacts, transform);
+
+        // Play sound effect
+        if (audioSource != null && audioClip != null)
+        {
+            audioSource.PlayOneShot(audioClip);
+        }
     }
 
-    public void OnHoverExited(HoverExitEventArgs args) {
+    public void OnHoverExited(HoverExitEventArgs args)
+    {
         HintPopup.Instance?.HideHint(transform);
     }
 
-    protected override void OnDisable() {
+    protected override void OnDisable()
+    {
         base.OnDisable();
         if (HintPopup.Instance != null)
         {
